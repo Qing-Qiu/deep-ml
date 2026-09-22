bcf81325957561f907e6db001ae6046e5379f088
Qing-Qiu <91710069+Qing-Qiu@users.noreply.github.com>
2026-09-22 15:22:39 +0000

    Solve: Matrix-Vector Dot Product

---
+++ problems/0001-matrix-vector-dot-product/solution.py
@@ -0,0 +1,12 @@
+def matrix_dot_vector(a: list[list[int|float]], b: list[int|float]) -> list[int|float]:
+	# Return a list where each element is the dot product of a row of 'a' with 'b'.
+	# If the number of columns in 'a' does not match the length of 'b', return -1.
+	ans = []
+	if (len(a[0]) != len(b)):
+		return -1
+	for x in a:
+		p = 0
+		for i in range(0, len(x)):
+			p += x[i] * b[i]
+		ans.append(p)
+	return ans
